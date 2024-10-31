c7c8358f53ee7fe795d05c2e9ce6e10a48a5af67
wajihasiddiqui <wajeehasiddiqui333@gmail.com>
2024-10-31 23:37:40 +0500

    localhostcahnge

--- src/app/feedback/feedback.service.ts
+++ src/app/feedback/feedback.service.ts
@@ -7,24 +7,9 @@ import { catchError, Observable, tap, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class FeedbackService {
-    private apiUrl = 'https://localhost:7154/api/Feedback';
-
-   constructor(private http: HttpClient) {}
-
+  constructor(private http: HttpClient) {}
+   private apiUrl = 'https://localhost:7130/api/Feedback';
   submitFeedback(feedback: any): Observable<any> {
     return this.http.post(this.apiUrl, feedback);
   }
-
-  // submitFeedback(feedback: any): Observable<any> {
-  //   console.log('Sending feedback:', feedback); // Log feedback data before sending
-  //   return this.http.post<any>('https://localhost:7154/api/feedback', feedback)
-  //     .pipe(
-  //       tap(response => console.log('Feedback submitted successfully:', response)), // Log success
-  //       catchError(error => {
-  //         console.error('Error occurred while submitting feedback:', error); // Log error
-  //         return throwError(error);
-  //       })
-  //     );
-  //   }
- 
 }
